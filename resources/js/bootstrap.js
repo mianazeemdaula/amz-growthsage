7f9815a7b94eb284e7c9277ba74f8b752f563f6b
mianazeemdaula <mianazeemdaula@gmail.com>
2023-07-22 19:23:17 +0500

    packg

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -30,3 +30,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
 //     enabledTransports: ['ws', 'wss'],
 // });
+
+import $ from 'jquery';
+window.$ = $;
+import { popper } from '@popperjs/core';
+window.Popper = popper;
