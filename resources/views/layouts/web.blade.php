4fb3aaa49b6e3ce51c73c54b649a50bcd19947ff
mianazeemdaula <mianazeemdaula@gmail.com>
2023-07-23 09:06:12 +0500

    build

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <script src="https://unpkg.com/phosphor-icons"></script>
+    <script src="https://code.iconify.design/iconify-icon/1.0.0/iconify-icon.min.js"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Growth Sage</title>
 </head>
@@ -1300,9 +1303,6 @@
         {{-- <script src="{{ asset("js/popper.min.js"></script> --}}"
         {{-- <script src="{{ asset("js/jquery-3.6.0.min.js"></script> --}}"
         {{-- <script src="{{ asset("js/rt-plugins.js"></script> --}}"
-        <script src="https://unpkg.com/phosphor-icons"></script>
-        <script src="https://code.iconify.design/iconify-icon/1.0.0/iconify-icon.min.js"></script>
-        {{-- <script src="{{ asset("js/app.js"></script> --}}"
     </body>
     @stack('js')
 </body>
