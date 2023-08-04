9b2d8ed94dd6f856ea3d30b19379e0117950b94e
mianazeemdaula <mianazeemdaula@gmail.com>
2023-08-04 19:19:56 +0500

    bas

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,46 +1,46 @@
 @extends('layouts.web')
 
 @section('header')
-<x-web.header></x-web.header>
+    <x-web.header></x-web.header>
 @endsection
 @section('body')
+    <div class="font-gilroy font-medium text-gray text-lg leading-[27px]">
 
-<div class="font-gilroy font-medium text-gray text-lg leading-[27px]">
-
-    <section class="px-4 md:px-32 xl:min-h-screen overflow-hidden">
-        <div class="absolute z-50 flex items-center h-screen w-full">
-            <div class="md:w-2/3 mt-24">
-                <h2>Amazon</h2>
-                <h1>
-                    <span class=" text-secondary inline-block bg-[url('../../public/images/banner/shape.svg')] bg-no-repeat bg-bottom">
-                        Product Hunting</span>
-                </h1>
-                <p class="text-slate-600 mt-8 text-2xl">Thoughtfully designed curriculum that combines industry insights and practical skills for you.</p>
-                <div class="flex-none mt-8">
-                    <a href="{{url('web/login')}}" class="btn btn-primary w-full lg:w-auto">Get Started Now</a>
+        <section class="px-4 md:px-32 xl:min-h-screen overflow-hidden">
+            <div class="absolute z-50 flex items-center h-screen w-full">
+                <div class="md:w-2/3 mt-24">
+                    <h2>Amazon</h2>
+                    <h1>
+                        <span
+                            class=" text-secondary inline-block bg-[url('../../public/images/banner/shape.svg')] bg-no-repeat bg-bottom">
+                            Product Hunting</span>
+                    </h1>
+                    <p class="text-slate-600 mt-8 text-2xl">Thoughtfully designed curriculum that combines industry insights
+                        and practical skills for you.</p>
+                    <div class="flex-none mt-8">
+                        <a href="{{ url('web/login') }}" class="btn btn-primary w-full lg:w-auto">Get Started Now</a>
+                    </div>
                 </div>
+
             </div>
 
-        </div>
+            <div class="imge-box absolute xl:right-[-260px]  hidden xl:block   bottom-0  ">
+                <img src="{{ asset('images/all-img/section-bg-1.png') }}" alt="">
+            </div>
+        </section>
 
-        <div class="imge-box absolute xl:right-[-260px]  hidden xl:block   bottom-0  ">
-            <img src="{{ asset('images/all-img/welcome/c1.jpg') }}" alt="">
-        </div>
-    </section>
+        <x-web.about></x-web.about>
+        <x-web.features></x-web.features>
+        <x-web.joinus></x-web.joinus>
+        <x-web.platform></x-web.platform>
+        <x-web.testimonial></x-web.testimonial>
+        <x-web.subscription></x-web.subscription>
+        <x-web.team></x-web.team>
+        <x-web.faq></x-web.faq>
 
-    <x-web.about></x-web.about>
-    <x-web.features></x-web.features>
-    <x-web.joinus></x-web.joinus>
-    <x-web.platform></x-web.platform>
-    <x-web.testimonial></x-web.testimonial>
-    <x-web.subscription></x-web.subscription>
-    <x-web.team></x-web.team>
-    <x-web.faq></x-web.faq>
-
-</div>
-
+    </div>
 @endsection
 
 @section('footer')
-<x-web.footer></x-web.footer>
+    <x-web.footer></x-web.footer>
 @endsection
