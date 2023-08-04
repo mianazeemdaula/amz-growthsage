ec66624f7cd32e42b16ab07073f5a406fd2626aa
abbas0506 <abbas.sscs@gmail.com>
2023-08-04 07:04:33 +0500

    040823

--- resources/views/components/student1/noprofile_msg.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="flex flex-col justify-center">
-    <div class="h2">Dear student!</div>
-    <div class="h3 mt-2">Your profile has been found incomplete.</div>
-    <p class="text-slate-600 leading-relaxed mt-2">It is one time process, however, it is necessary to proceed next . Please take a while to complete your profile. Once you complete your profile, courses list will be available to you for registration. </p>
-</div>
