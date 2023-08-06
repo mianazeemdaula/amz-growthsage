3e184d91ff7e999efb1a5c57c7794afb7eabecd3
mianazeemdaula <mianazeemdaula@gmail.com>
2023-08-06 13:54:16 +0500

    as

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     if (Auth::check()) {
-        if (Auth::user()->hasRole('student'))
+        if (Auth::user()->hasRole('student|teamleader'))
             return redirect('students');
         if (Auth::user()->hasRole('admin'))
             return redirect('admin');
